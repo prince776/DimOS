fd2440d6ad293bcd871fed0a44d739753937d1c3
Prince Gupta <21282111+prince776@users.noreply.github.com>
2022-12-07 12:14:10 +0530

    Basic setup done, global constructors working

--- iso.sh
+++ iso.sh
@@ -2,14 +2,32 @@
 set -e
 . ./build.sh
 
-mkdir -p isodir
-mkdir -p isodir/boot
-mkdir -p isodir/boot/grub
+###### OLD GRUB #####
+# mkdir -p isodir
+# mkdir -p isodir/boot
+# mkdir -p isodir/boot/grub
 
-cp sysroot/boot/dimos.kernel isodir/boot/dimos.kernel
-cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "dimos" {
-	multiboot /boot/dimos.kernel
-}
-EOF
-grub-mkrescue -o dimos.iso isodir
+# cp sysroot/boot/dimos.kernel isodir/boot/dimos.kernel
+# cat > isodir/boot/grub/grub.cfg << EOF
+# menuentry "dimos" {
+# 	multiboot /boot/dimos.kernel
+# }
+# EOF
+# grub-mkrescue -o dimos.iso isodir
+
+# Download the latest Limine binary release.
+make -C limine
+mkdir -p isodir
+
+cp -v sysroot/boot/dimos.kernel limine.cfg limine/limine.sys \
+      limine/limine-cd.bin limine/limine-cd-efi.bin isodir/
+ 
+# Create the bootable ISO.
+xorriso -as mkisofs -b limine-cd.bin \
+        -no-emul-boot -boot-load-size 4 -boot-info-table \
+        --efi-boot limine-cd-efi.bin \
+        -efi-boot-part --efi-boot-image --protective-msdos-label \
+        isodir -o dimos.iso
+ 
+# Install Limine stage 1 and 2 for legacy BIOS boot.
+./limine/limine-deploy dimos.iso
--- target-triplet-to-arch.sh
+++ target-triplet-to-arch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-if echo "$1" | grep -Eq 'i[[:digit:]]86-'; then
+if echo "$1" | grep -Eq 'x86_64-'; then
+  echo x86_64
+elif echo "$1" | grep -Eq 'i[[:digit:]]86-'; then
   echo i386
 else
   echo "$1" | grep -Eo '^[[:alnum:]_]*'
